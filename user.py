5a49c7969bc8c4a66835bba34122b621129f0888
adragon9 <adragon9@outlook.com>
2024-04-18 01:23:32 -0500

    renamed from security.py to user.py because security did not describe what I intended for the class.

--- user.py
+++ user.py
@@ -4,23 +4,24 @@ import tkinter as tk
 from tkinter import ttk
 
 
-class Security:
-    def __init__(self, user: str, password: str, user_entry: tk.Entry, pass_entry: tk.Entry, tabs: ttk.Notebook):
+class User:
+    def __init__(self, username: str, password: str, user_entry: tk.Entry, pass_entry: tk.Entry, tabs: ttk.Notebook):
         """
         user_entry and pass_entry are the entry boxes that contain the username and password.
         tabs is the Notebook that has your tabs in it.
-        :param user:
+        :param username:
         :param password:
         :param user_entry:
         :param pass_entry:
         :param tabs:
         """
-        self.current_user = None
-        self.user = user
+        self.user = username
+        self.current_user = username
         self.password = password
         self.user_entry = user_entry
         self.pass_entry = pass_entry
         self.tabs = tabs
+        self.balance = 0.0
 
     def create_user(self):
         connection = sqlite3.connect("Users.db")
@@ -37,17 +38,18 @@ class Security:
             CREATE TABLE IF NOT EXISTS users(
             user_name STRING PRIMARY KEY,
             date_created DATETIME,
-            password STRING)""")
+            password STRING,
+            balance MONEY)""")
 
             date = dt.datetime.now()
-            user = self.user
-            password = self.password
+
             try:
                 cursor.execute("""
                 INSERT INTO users(
                 user_name,
                 date_created,
-                password) VALUES(?,?,?)""", (user, date, password))
+                password,
+                balance) VALUES(?,?,?,?)""", (self.user, date, self.password, self.balance))
             except sqlite3.IntegrityError:
                 print("USERNAME TAKEN")
 
@@ -58,6 +60,7 @@ class Security:
             for row in rows:
                 print(row)
 
+        connection.close()
         self.user_entry.delete(0, tk.END)
         self.pass_entry.delete(0, tk.END)
 
@@ -66,12 +69,12 @@ class Security:
         cursor = connection.cursor()
 
         if self.user == '':
-            user = None
+            self.user = None
         elif not self.user.isalpha():
-            user = None
+            self.user = None
 
         if self.password == '':
-            password = None
+            self.password = None
 
         if self.user is not None and self.password is not None:
             cursor.execute((f"""SELECT
@@ -87,12 +90,35 @@ class Security:
                 tab_count = self.tabs.index('end')
                 for i in range(1, tab_count):
                     self.tabs.tab(i, state="normal")
-                    self.current_user = check
+                    print(check[0])
+                    self.current_user = check[0]
             else:
                 print("no match")
 
+        connection.close()
         self.user_entry.delete(0, tk.END)
         self.pass_entry.delete(0, tk.END)
 
     def get_current_user(self):
         return self.current_user
+
+    def get_income(self):
+        return self.balance
+
+    def set_balance(self, balance):
+        connection = sqlite3.connect("Users.db")
+        cursor = connection.cursor()
+        try:
+            self.balance = float(balance)
+        except ValueError:
+            self.balance = 0.0
+
+        print(self.user)
+        print(self.current_user)
+
+        cursor.execute("""UPDATE users
+        SET balance = ?
+        WHERE user_name = ?""", (self.balance, self.current_user))
+
+        connection.commit()
+        connection.close()
